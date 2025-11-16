8856a7f0f9378cd6da4ada54077b0cf32645e542
Siam-Kabir-2 <mdsiamkabir1@gmail.com>
2025-11-16 23:45:49 +0600

    Welcome Scene created and applied switching.

--- src/main/java/com/example/learning/HelloController.java
+++ src/main/java/com/example/learning/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.learning;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button createBtn;
+
+    @FXML
+    public void onCreateBtnClk() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("form-view.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        Stage stage = (Stage) createBtn.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
+
+
+
+
+
 }
